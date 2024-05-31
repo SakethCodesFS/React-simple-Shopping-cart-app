2f18a5862c800d14bf113e78b6d264d2fc11a9be
Sai Saketh <141397642+SakethCodesFS@users.noreply.github.com>
2024-05-31 00:47:20 -0500

    Added theming to the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF5722', // Warm orange
+        secondary: '#795548', // Brown
+        accent: '#FF9800', // Lighter orange
+        neutral: '#FFFFFF', // White
+        dark: '#212121' // Dark grey
+      }
+    },
   },
   plugins: [],
 }
